d23c7eba05bd97872258b7c57dfe2b91dbdd6bc8
Angel <sarrom95@gmail.com>
2022-02-02 00:15:04 +0100

    Refactorizando el switch

--- js/cube.js
+++ js/cube.js
@@ -1,6 +1,7 @@
 // Variables globales
 let area;
 let cube;
+let acctions = [];
 // Área para el proyecto
 let main = document.getElementsByTagName("main")[0];
 area = document.createElement("div");
@@ -9,6 +10,13 @@ area.style.border = "2px solid red";
 area.style.height = "400px";
 area.style.position = "relative";
 main.parentElement.insertBefore(area, main);
+let divInfo = document.createElement("div");
+main.parentElement.insertBefore(divInfo, main);
+
+area.addEventListener('mouseenter', (event) =>{
+	// En divInfo tenemos que pegar las coordenadas
+})
+
 // Pieza que queremos mover
 cube = document.createElement("div");
 cube.style.background = "red";
@@ -22,7 +30,7 @@ area.appendChild(cube);
 
 document.addEventListener("keydown", function (event) {
 	console.log(event.code);
-	switch (event.code){
+	switch (event.code) {
 		case "ArrowUp":
 			addAction("up");
 			break;
@@ -47,42 +55,40 @@ document.addEventListener("keydown", function (event) {
 	event.preventDefault();
 });
 
-function moveUp(cube){
+function moveUp(cube) {
 	let top = cube.offsetTop;
 	top -= 10;
-	top = (top < 0)? 0 : top;
+	top = (top < 0) ? 0 : top;
 	cube.style.top = top + "px";
 }
-function moveDown(cube){
+function moveDown(cube) {
 	let top = cube.offsetTop;
 	top += 10;
-	top = (top > area.offsetHeight - cube.offsetHeight)? area.offsetHeight
+	top = (top > area.offsetHeight - cube.offsetHeight) ? area.offsetHeight
 		- cube.offsetHeight : top;
 	cube.style.top = top + "px";
 }
-function moveLeft(cube){
+function moveLeft(cube) {
 	let left = cube.offsetLeft;
 	left -= 10;
-	left = (left < 0)? 0 : left;
+	left = (left < 0) ? 0 : left;
 	cube.style.left = left + "px";
 }
-function moveRight(cube){
+function moveRight(cube) {
 	let left = cube.offsetLeft;
 	left += 10;
-	left = (left > area.offsetWidth - cube.offsetWidth)? area.offsetWidth -
+	left = (left > area.offsetWidth - cube.offsetWidth) ? area.offsetWidth -
 		cube.offsetWidth : left;
 	cube.style.left = left + "px";
 }
-function randomColor(cube){
+function randomColor(cube) {
 	let r = Math.floor((Math.random() * 256));
 	let g = Math.floor((Math.random() * 256));
 	let b = Math.floor((Math.random() * 256));
 	cube.style.background = `rgb(${r}, ${g}, ${b})`;
 }
 
-let acctions = [];
-
-function addAction(action){
+function addAction(action) {
 	let span = document.createElement("span");
 	acctions.push({
 		action: action,
@@ -98,38 +104,41 @@ function addAction(action){
 	span.addEventListener("mouseenter", function () {
 		this.style.backgroundColor = "red";
 		this.style.color = "white";
-	})
+	});
 	span.addEventListener("mouseleave", function () {
 		this.style.backgroundColor = "white";
 		this.style.color = "black";
-	})
+	});
+	span.addEventListener('click', function(){
+		let index = acctions.findIndex((action) => {
+			console.log(action.span);
+			console.log(this);
+			return (action.span == this);
+		});
+		console.log(index);
+		acctions.splice(index,1);
+		this.remove();
+	});
 	area.appendChild(span);
 }
 
-function executeAcctions(){
-	if (acctions.length > 0){
+let mapActions = new Map([
+	["up", moveUp],
+	["down",moveDown],
+	["left",moveLeft],
+	["right",moveRight],
+	["color",randomColor]
+])
+
+function executeAcctions() {
+	if (acctions.length > 0) {
 		let action = acctions.shift();
-		switch(action.action){
-			case "up":
-				moveUp(cube);
-				break;
-			case "down":
-				moveDown(cube);
-				break;
-			case "left":
-				moveLeft(cube);
-				break;
-			case "right":
-				moveRight(cube);
-				break;
-			case "color":
-				randomColor(cube);
-				break;
-			default:
-				break;
-		}
+		mapActions.get(action.action).call(null, cube);
 		action.span.remove();
 		setTimeout(executeAcctions, 50);
+		console.log("retorno: ");
+	} else{
+		console.log("Fin de la recursividad");
 	}
 }
 
@@ -137,7 +146,7 @@ span.addEventListener("click", function () {
 	let index = acctions.findIndex((action) => {
 		return action.span === this;
 	})
-	acctions.splice(index,1);
+	acctions.splice(index, 1);
 	this.remove();
 })
 
